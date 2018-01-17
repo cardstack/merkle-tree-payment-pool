8e355529f7efabc8b0d17259f8d516eade565bb4
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2018-01-17 14:46:04 -0500

    plumbing for using merkle trees in payment pool

--- contracts/PaymentPool.sol
+++ contracts/PaymentPool.sol
@@ -3,11 +3,13 @@ pragma solidity ^0.4.18;
 import 'zeppelin-solidity/contracts/math/SafeMath.sol';
 import 'zeppelin-solidity/contracts/token/ERC20.sol';
 import 'zeppelin-solidity/contracts/token/SafeERC20.sol';
+import 'zeppelin-solidity/contracts/MerkleProof.sol';
 
 contract PaymentPool {
 
   using SafeMath for uint256;
   using SafeERC20 for ERC20;
+  using MerkleProof for bytes;
 
   struct AnalyticsSolution {
     uint256 epochNumber;
@@ -20,23 +22,15 @@ contract PaymentPool {
   uint256 constant MIN_GAS_BAIL_THRESHOLD = 50000;
 
   ERC20 public token;
-  uint256 public payeeIndex;
   uint256 public numEpochs;
   mapping(address => uint256) public miningStake;
 
   uint256 currentEpochStartBlock;
-  uint256 settlementId;
-  uint256 currentQueueJobId;
-  mapping(uint256 => AnalyticsSolution) settlementQueue;
+  bytes32 payeeRoot;
 
-  // Fired when the payee disbursement cannot be made because paymentpool has been depleted.
-  // Analytics will need to include this payee in next epoch
-  event SkippedPayee(address indexed payee, uint256 amount, uint256 epochNumber);
-  event ProcessedPayee(address indexed payee, uint256 amount, uint256 epochNumber);
-  event EpochEnded(uint256 epochNumber, uint256 startBlock, uint256 endBlock);
-  event AnalyticsSubmitted(uint256 epochNumber, address[] payees, uint256[] amounts, address miner, bytes32 proof);
   event MiningStake(address indexed miner, uint256 amount);
-  event CompletedSettlementJob(uint256 settlementId, uint256 epochNumber);
+  event EpochEnded(uint256 epochNumber, uint256 startBlock, uint256 endBlock);
+  event PayeeMerkleRoot(bytes32 root /*, uint256 epochNumber*/);
 
   event Debug(string msg, uint256 value);
 
@@ -46,7 +40,7 @@ contract PaymentPool {
   }
 
   // miner needs to first perform an ERC-20 approve of the mining pool so that we can perform a transferFrom of the token
-  function postMiningStake(uint256 amount) public {
+  function postMiningStake(uint256 amount) public returns (bool) {
     require(token.allowance(msg.sender, this) >= amount);
 
     token.safeTransferFrom(msg.sender, this, amount);
@@ -54,9 +48,11 @@ contract PaymentPool {
     miningStake[msg.sender] = amount;
 
     MiningStake(msg.sender, amount);
+
+    return true;
   }
 
-  function startNewEpoch() public  {
+  function startNewEpoch() public returns(bool) {
     // TODO don't start new epoch if the n - 1 epoch is still unsolved
     // at most there can be 1 unsolved epoch
     require(block.number > currentEpochStartBlock);
@@ -65,66 +61,16 @@ contract PaymentPool {
 
     numEpochs = numEpochs.add(1);
     currentEpochStartBlock = block.number.add(1);
+
+    return true;
   }
 
-  // analytics submitted are for the n-1 epoch
-  // idea: payment pool could govern a basket of tokens...
-  function submitAnalytics(address[] payees, uint256[] amounts, bytes32 proof) public {
-    // confirm proof? (could be very expensive)...
-    // TODO need to cap the number of payees so we don't run out of gas updating the contract state
-    // TODO confirm msg.sender is an approved miner for this epoch
+  function submitPayeeMerkleRoot(bytes32 _payeeRoot) public returns(bool) {
+    payeeRoot = _payeeRoot;
 
-    require(miningStake[msg.sender] > 0); // this is a simple check, eventually we need to do something much more thorough
-    require(payees.length > 0);
-    require(payees.length == amounts.length);
+    PayeeMerkleRoot(_payeeRoot);
 
-    settlementQueue[settlementId] = AnalyticsSolution({
-      epochNumber: numEpochs.sub(1),
-      payees: payees,
-      amounts: amounts,
-      miner: msg.sender,
-      proof: proof
-    });
-    settlementId = settlementId.add(1);
-
-    AnalyticsSubmitted(numEpochs.sub(1), payees, amounts, msg.sender, proof); // the amounts emitted are an array of hexidecimal in web3
+    return true;
   }
 
-  function getSettlementQueueJob(uint256 _settlementId) public view returns (uint256 _epochNumber,
-                                                                             address[] _payees,
-                                                                             uint256[] _amounts,
-                                                                             address _miner,
-                                                                             bytes32 _proof) {
-    AnalyticsSolution storage job = settlementQueue[_settlementId];
-    _epochNumber = job.epochNumber;
-    _payees = job.payees;
-    _amounts = job.amounts;
-    _miner = job.miner;
-    _proof = job.proof;
-  }
-
-  // breaking out the function to dispurse payments from the pool as it will require
-  // mulitple blocks to process
-  function disbursePayments() public returns (bool) {
-    AnalyticsSolution storage job = settlementQueue[currentQueueJobId];
-    if (job.payees.length == 0) return true;
-
-    while (payeeIndex < job.payees.length && msg.gas > MIN_GAS_BAIL_THRESHOLD) {
-      if (token.balanceOf(this) >= job.amounts[payeeIndex]) {
-        token.transfer(job.payees[payeeIndex], job.amounts[payeeIndex]);
-        ProcessedPayee(job.payees[payeeIndex], job.amounts[payeeIndex], job.epochNumber);
-      } else {
-        SkippedPayee(job.payees[payeeIndex], job.amounts[payeeIndex], job.epochNumber);
-      }
-      payeeIndex = payeeIndex.add(1);
-    }
-
-    // vv Everything below here needs to be able to run with MIN_GAS_BAIL_THRESHOLD budget vv
-    if (payeeIndex >= job.payees.length) {
-      CompletedSettlementJob(currentQueueJobId, job.epochNumber);
-
-      payeeIndex = 0;
-      currentQueueJobId = currentQueueJobId.add(1);
-    }
-  }
 }
--- test/payment-pool-test.js
+++ test/payment-pool-test.js
@@ -1,33 +1,45 @@
+import MerkleTree from '../lib/merkle-tree';
+import { sha3, bufferToHex } from 'ethereumjs-util';
+
 const PaymentPool = artifacts.require('./PaymentPool.sol');
 const Token = artifacts.require('./Token.sol');
+
+function createPaymentLeafNodes(payments) {
+  return payments.map(payment => payment.payee + payment.amount);
+}
 
 contract('PaymentPool', function(accounts) {
   let paymentPool;
   let token;
   let miner = accounts[1];
-  let payees = [
-    accounts[1],
-    accounts[2],
-    accounts[3],
-    accounts[4],
-    accounts[5],
-    accounts[6],
-    accounts[7],
-    accounts[8],
-    accounts[9]
-  ];
-
-  let amounts = [
-    10,
-    12,
-    2,
-    1,
-    32,
-    10,
-    9,
-    9,
-    2
-  ];
+  let payments = [{
+    payee: accounts[1],
+    amount: 10
+  },{
+    payee: accounts[2],
+    amount: 12
+  },{
+    payee: accounts[3],
+    amount: 2,
+  },{
+    payee: accounts[4],
+    amount: 1
+  },{
+    payee: accounts[5],
+    amount: 32
+  },{
+    payee: accounts[6],
+    amount: 10
+  },{
+    payee: accounts[7],
+    amount: 9
+  },{
+    payee: accounts[8],
+    amount: 9
+  },{
+    payee: accounts[9],
+    amount: 2
+  }];
 
   describe("ledger", function() {
     beforeEach(async function() {
@@ -37,95 +49,21 @@ contract('PaymentPool', function(accounts) {
       await token.approve(paymentPool.address, 100, { from: miner });
     });
 
-    xit("can iterate thru payees", async function() {
-      let txn = await paymentPool.submitAnalytics(payees, amounts, {
-        gas: 100000
-      });
-
-      let payeeIndex = await paymentPool.payeeIndex();
-
-      console.log(JSON.stringify(txn, null, 2));
-      console.log("PAYEE LOGS:", txn.logs.length);
-      console.log("PAYEE INDEX:", payeeIndex.toNumber());
-
-      txn = await paymentPool.submitAnalytics(payees, amounts, {
-        gas: 100000
-      });
-
-      payeeIndex = await paymentPool.payeeIndex();
-
-      console.log(JSON.stringify(txn, null, 2));
-      console.log("PAYEE LOGS:", txn.logs.length);
-      console.log("PAYEE INDEX:", payeeIndex.toNumber());
-    });
-
-    it("can allow a miner to post stake with the payment pool", async function() {
-      let txn = await paymentPool.postMiningStake(10, { from: miner });
-      assert.equal(txn.logs.length, 1, "the number of events is correct");
-
-      let event = txn.logs[0];
-      assert.equal(event.event, "MiningStake", "the event type is correct");
-      assert.equal(event.args.miner, miner, "the miner is correct");
-      assert.equal(event.args.amount.toNumber(), 10, "the amount staked is correct");
-
-      let stake = await paymentPool.miningStake(miner);
-
-      assert.equal(stake.toNumber(), 10, "the mining stake is correct");
-    });
-
-    it("can store the analytics solution", async function() {
-      await paymentPool.postMiningStake(10, { from: miner });
-      await paymentPool.startNewEpoch();
-
-      let proof = web3.toHex("my proof is pudding");
-      let txn = await paymentPool.submitAnalytics(payees, amounts, proof, { from: accounts[1] });
-
-      console.log(JSON.stringify(txn, null, 2));
-
-      txn = await paymentPool.getSettlementQueueJob(0);
-
-      console.log("ANALYTICS SOLUTION", JSON.stringify(txn, null, 2));
-    });
-
-    it.only("can disburse payments", async function() {
+    it.only("payees can withdraw from pool", async function() {
       let miningStake = 10;
       let paymentPoolBalance = 100;
+      let paymentElements = createPaymentLeafNodes(payments);
+      let merkleTree = new MerkleTree(paymentElements);
+      let root = merkleTree.getHexRoot();
+
       await token.mint(paymentPool.address, paymentPoolBalance);
       await paymentPool.postMiningStake(miningStake, { from: miner });
       await paymentPool.startNewEpoch();
-      let proof = web3.toHex("my proof is pudding");
-      await paymentPool.submitAnalytics(payees, amounts, proof, { from: accounts[1] });
 
-
-      let txn = await paymentPool.disbursePayments();
+      let txn = await paymentPool.submitPayeeMerkleRoot(root);
 
       console.log(JSON.stringify(txn, null, 2));
 
-      // note payees[1] is also the miner
-      let balance = await token.balanceOf(payees[2]);
-      assert.equal(balance.toNumber(), amounts[2], "balance is correct");
-
-      balance = await token.balanceOf(payees[3]);
-      assert.equal(balance.toNumber(), amounts[3], "balance is correct");
-
-      balance = await token.balanceOf(payees[4]);
-      assert.equal(balance.toNumber(), amounts[4], "balance is correct");
-
-      balance = await token.balanceOf(payees[5]);
-      assert.equal(balance.toNumber(), amounts[5], "balance is correct");
-
-      balance = await token.balanceOf(payees[6]);
-      assert.equal(balance.toNumber(), amounts[6], "balance is correct");
-
-      balance = await token.balanceOf(payees[7]);
-      assert.equal(balance.toNumber(), amounts[7], "balance is correct");
-
-      balance = await token.balanceOf(payees[8]);
-      assert.equal(balance.toNumber(), amounts[8], "balance is correct");
-
-      let amountsTotal = amounts.reduce((total, amount) => total + amount);
-      balance = await token.balanceOf(paymentPool.address);
-      assert.equal(balance.toNumber(), paymentPoolBalance - amountsTotal + miningStake, "balance is correct");
     });
 
   });
