5b2cbff698ad6a261980d7ad58b00160e1608328
tintinthong <justinthong93@gmail.com>
2021-05-03 12:46:35 +0800

    add network for ganache/testrpc

--- truffle.js
+++ truffle.js
@@ -4,4 +4,11 @@ require('babel-polyfill');
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  networks: {
+    test: {
+      host: "localhost", // Localhost (default: none)
+      port: 8545, // Standard Ethereum port (default: none)
+      network_id: "*", // Any network (default: none)
+    },
+  }
 };
