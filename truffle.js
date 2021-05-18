b185ea8f4ccaf91535110ada02cecec81a3c3fff
Justin <justinthong93@gmail.com>
2021-05-18 14:01:00 +0800

    Remove cruft
    delete migrationt script
    
    delete truffle.js

--- truffle.js
+++
@@ -1,7 +0,0 @@
-require('babel-register');
-require('babel-polyfill');
-
-module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
-};
